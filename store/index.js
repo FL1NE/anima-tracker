8d6a056fe5c3369ba4b97e79ebaa4cecc634c60b
Tomoki FL1NE SHISHIKURA <tomoki.shishikura@gmail.com>
2022-05-02 19:06:58 +0900

    chore: add memo

--- store/index.js
+++ store/index.js
@@ -1,5 +1,13 @@
 export const state = () => ({
   inputHistory: [],
+  // いずれこんな感じでとる
+  // [
+  //   {
+  //     memo: "",
+  //     createdAt: (date),
+  //     data: [1, 2, 3, 4, 5, 6, 7, 8],
+  //   }
+  // ]
 })
 
 export const mutations = {
